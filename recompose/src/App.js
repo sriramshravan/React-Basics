36f6a11bbc179c3cfbd8331a36e37b34a2ae3cf9
sriramshravan <sriram.shravan@omniwyse.com>
2019-05-06 10:26:18 +0530

    recompose commit

---
+++ recompose/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SignupForm from "./SignupForm";
+
+function App() {
+  return (
+    <div className="App">
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
